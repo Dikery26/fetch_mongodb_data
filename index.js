dc723eabd3f1a55e809f93b2f8deb9d323f84e15
Dikery26 <dikery@socialscript.in>
2024-08-23 13:00:11 +0530

    pagination

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors({
 // Function to connect to MongoDB
 async function connectToMongoDB() {
   try {
-    const client = new MongoClient(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+    const client = new MongoClient(mongoUrl);
     await client.connect();
     db = client.db(dbName);
     console.log('Connected to MongoDB');
@@ -29,30 +29,42 @@ async function connectToMongoDB() {
 }
 
 app.get('/getContents', async (req, res) => {
-    try {
+  try {
       const collectionName = 'All-in-One_Alerts'; // Replace with your collection name
       const collection = db.collection(collectionName);
-      const documents = await collection.find({}).toArray();
-  
+
+      // Pagination parameters
+      const page = parseInt(req.query.page) || 1;
+      const limit = parseInt(req.query.limit) || 10;
+
+      // Calculate the number of documents to skip
+      const skip = (page - 1) * limit;
+
+      // Fetch paginated data
+      const documents = await collection.find({})
+          .skip(skip)
+          .limit(limit)
+          .toArray();
+
       // Map through the documents and add an `imageSrc` property for Base64 images
       const documentsWithImages = documents.map(doc => {
-        if (doc.attachments && doc.attachments.length > 0) {
-          doc.attachments = doc.attachments.map(attachment => {
-            return {
-              ...attachment,
-              imageSrc: `data:${attachment.mimeType};base64,${attachment.data.toString('base64')}`
-            };
-          });
-        }
-        return doc;
+          if (doc.attachments && doc.attachments.length > 0) {
+              doc.attachments = doc.attachments.map(attachment => {
+                  return {
+                      ...attachment,
+                      imageSrc: `data:${attachment.mimeType};base64,${attachment.data.toString('base64')}`
+                  };
+              });
+          }
+          return doc;
       });
-  
+
       res.json(documentsWithImages);
-    } catch (error) {
+  } catch (error) {
       res.status(500).json({ error: 'Error fetching data' });
-    }
-  });
-  
+  }
+});
+
 
 // Start the server
 app.listen(port, async () => {
