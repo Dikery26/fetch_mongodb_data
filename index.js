3d8d066dee74754528ab7de1831fe9db57240e14
Dikery26 <dikery@socialscript.in>
2024-08-27 14:35:11 +0530

    Filter

--- index.js
+++ index.js
@@ -69,12 +69,18 @@ app.get('/getContents/Crypto_Alerts', async (req, res) => {
       const page = parseInt(req.query.page) || 1;
       const limit = parseInt(req.query.limit) || 10;
       const skip = (page - 1) * limit;
+      const date = req.query.date; // New parameter for date filtering
 
       const collectionName = 'Crypto_Alerts';
       const collection = db.collection(collectionName);
 
-      const documents = await collection.find({})
-          .sort({ receiveTime: -1 }) // Sort by receiveTime in descending order
+      let query = {};
+      if (date) {
+          query = { subject: new RegExp(`for ${date}`, 'i') };
+      }
+
+      const documents = await collection.find(query)
+          .sort({ receiveTime: -1 })
           .skip(skip)
           .limit(limit)
           .toArray();
